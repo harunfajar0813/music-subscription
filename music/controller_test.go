ae56ac69ff90e393893ae49ce4b477b997e28580
harunfajar0813 <spam.harun0813@gmail.com>
2020-01-18 14:05:56 +0700

    controller testing

---
+++ music/controller_test.go
@@ -0,0 +1,57 @@
+package music
+
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+const createSubscriptionTable = `
+CREATE TABLE IF NOT EXISTS subscriptions
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    price INT NOT NULL,
+	duration INT NOT NULL
+)`
+
+var a App
+
+func TestMain(m *testing.M) {
+	a = App{}
+	a.Initialize("root", "", "bcc_music")
+
+	ensureSubscriptionTableExists()
+
+	code := m.Run()
+
+	clearSubscriptionTable()
+
+	os.Exit(code)
+}
+
+func ensureSubscriptionTableExists() {
+	if _, err := a.DB.Exec(createSubscriptionTable); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func clearSubscriptionTable() {
+	a.DB.Exec("DELETE FROM subscriptions")
+	a.DB.Exec("ALTER TABLE subscriptions AUTO_INCREMENT = 1")
+}
+
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	a.Router.ServeHTTP(rr, req)
+
+	return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
